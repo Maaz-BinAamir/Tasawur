61028f0510daffbd120e9f34fa509c57f088ca70
Maaz Bin Aamir <maamir.bese23seecs@seecs.edu.pk>
2024-11-23 23:39:23 +0500

    Revert "Added Backend and implemented basic login system"
    This reverts commit 8d7e044221d81b248ab2bc340b1f0592b132b951.

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CreateUserView, LoginView
-
-urlpatterns = [
-    path('signup/', CreateUserView.as_view(), name='create_user'),
-    path('login/', LoginView.as_view(), name='create_user'),
-]
